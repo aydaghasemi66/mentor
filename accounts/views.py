6c7c73b22fc990db5558764b6b5b73afbee3d0dc
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2024-08-05 01:42:22 +0330

    login and sign up (django defaulf)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+from root.views import home
+from django.contrib.auth import login as auth_login
 # Create your views here.
+
+
+def signupview(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def loginview(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'root',
+    'accounts',
+    'course',
+    
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = 'home'  
+LOGOUT_REDIRECT_URL = 'home'  
