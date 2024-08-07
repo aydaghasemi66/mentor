8f5a2bcf4de32180c4d9218c85bac491399135be
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2024-08-07 17:12:25 +0330

    accounts user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
+class CustomBaseUserManager(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        if not username:
+            raise ValueError('Users must have a username')
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    def create_superuser(self,email,username,**extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self.create_user(email, username, **extra_fields)
+            
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomBaseUserManager()
+
+
+    def __str__(self):
+        return self.email
