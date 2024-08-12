cda1f95729265207f2e6d18090e52d5756a13428
Ayda <136740721+aydaghasemi66@users.noreply.github.com>
2024-08-12 16:58:53 +0330

    customeuser model and panel admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
-from .models import CustomUser
-# Register your models here.
-admin.site.register(CustomUser)
+from .models import CustomeUser
+from django.contrib.auth.admin import UserAdmin
+
+class CustomeUserAdmin(UserAdmin):
+    list_display = ("email", "is_staff", "is_active", "is_superuser", "is_verified")
+    list_filter = ("is_staff", "is_active", "is_superuser", "is_verified")
+    
+
+    fieldsets = (
+        ("Basic data", {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("username",)}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "is_verified")}),
+        ("Important dates", {"fields": ("last_login",)}),  
+    )
+    
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "password1", "password2"),
+        }),
+    )
+    
+    readonly_fields = ('created_date','updated_date')  
+
+admin.site.register(CustomeUser, CustomeUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,42 +1,47 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-# Create your models here.
-class CustomBaseUserManager(BaseUserManager):
-    def create_user(self, username, email, password, **extra_fields):
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin,)
+
+
+class CustomeBaseUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('Users must have an email address')
-        if not username:
-            raise ValueError('Users must have a username')
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            **extra_fields,
-        )
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
-    def create_superuser(self,email,username,**extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True')
-        return self.create_user(email, username, **extra_fields)
-            
-class CustomUser(AbstractBaseUser):
-    email = models.EmailField(max_length=255)
-    username = models.CharField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_verified") is not True:
+            raise ValueError("Superuser must have is_verified=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomeUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-    date_joined = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    USERNAME_FIELD = 'email'
+    is_verified = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomBaseUserManager()
-
+    objects = CustomeBaseUserManager()
 
     def __str__(self):
         return self.email
+
+
